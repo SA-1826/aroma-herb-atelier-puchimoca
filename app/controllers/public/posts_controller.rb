1ca7ee47ca62477e9749900d3b243e1f061abe0c
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2024-12-22 01:19:04 +0000

    [Update]ポートフォリオ 投稿コントローラー

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -8,19 +8,30 @@ class Public::PostsController < ApplicationController
     @post.user_id = current_user.id
     if @post.save
       flash[:notice] = "投稿しました"
-      redirect_to post_path(@post.id)
+      redirect_to post_path(@post)
     else
+      flash.now[:danger] = "投稿に失敗しました"
       render :new
     end
   end
 
   def index
+    @posts = Post.all
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update
+      flash[:notice] = "更新しました"
+      redirect_to post_path(@post)
+    else
+      flash.now[:danger] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def show
