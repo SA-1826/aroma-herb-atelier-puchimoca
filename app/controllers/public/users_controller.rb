a107cb63f39d72edcf94981a9c2e35b17943c6f9
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2024-12-29 15:43:50 +0000

    [Update]ポートフォリオ mypage2

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,7 @@
 class Public::UsersController < ApplicationController
   def mypage
-    @user = User.find(current_user)
-    @posts = current_user.posts.all
+    @user = current_user
+    @posts = @user.posts.all
   end
 
   def edit
