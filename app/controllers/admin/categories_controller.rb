767ff2d7c8a10bd6848426bed99a7575ed4f8979
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-01-14 02:59:36 +0000

    [Add]ポートフォリオ admin-categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,10 +2,20 @@ class Admin::CategoriesController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
   
-  def new
+  def index
+    @category = Category.new
+    @categories = Category.find(params[:id])
   end
 
-  def index
+  def create
+    @category = current_admin.categories.builds(category_params)
+    if @category.save
+      flash[:notice] = "カテゴリーを登録しました"
+      redirect_to admin_categories_path
+    else
+      flash.now[:danger] = "カテゴリーの登録に失敗しました"
+      render admin_categories_path
+    end
   end
 
   def edit
