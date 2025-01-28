fedddaa2fce5a219315a9529b67596260591826c
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-01-28 16:47:18 +0000

    [Add]user-programs index.html.erb, layouts/new_programs.html.erb, new_programs関連(homes,application)

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,7 +2,6 @@ class Public::PostsController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def new
-    
     @post = Post.new
   end
 
--- app/controllers/public/programs_controller.rb
+++ app/controllers/public/programs_controller.rb
@@ -1,7 +1,10 @@
 class Public::ProgramsController < ApplicationController
   def index
+    @programs = Program.all
+    @new_programs = Program.published.order(created_at: :desc).limit(3)
   end
 
   def show
+    @program = Program.find(params[:id])
   end
 end
