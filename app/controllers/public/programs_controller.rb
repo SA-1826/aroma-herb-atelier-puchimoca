7116fd488384581d2e8acbf12898060a1ba3e156
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-01-30 10:52:41 +0000

    [Update]user-programs show.html.erb, title-bg-color

--- app/controllers/public/programs_controller.rb
+++ app/controllers/public/programs_controller.rb
@@ -6,5 +6,6 @@ class Public::ProgramsController < ApplicationController
 
   def show
     @program = Program.find(params[:id])
+    @new_programs = Program.published.order(created_at: :desc).limit(3)
   end
 end
