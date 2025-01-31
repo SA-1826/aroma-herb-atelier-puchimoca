0727d5a3309b31cbd8b2868a6b76b333c53bced5
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-01-31 15:53:29 +0000

    [Add]admin-users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,9 +2,13 @@ class Admin::UsersController < ApplicationController
   layout 'admin'
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.all
+    @comments = @user.comments.all
   end
 
   def edit
