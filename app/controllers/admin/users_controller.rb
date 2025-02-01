8bb53671adc72e4b7cee2ed00c5473269f4372dd
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-02-01 10:53:18 +0000

    [Update]admin-users edit(is_active)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,8 +16,22 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "変更を保存しました"
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:danger] = "変更の保存に失敗しました"
+      render :edit
+    end
   end
 
   def withdraw
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :is_active)
   end
 end
