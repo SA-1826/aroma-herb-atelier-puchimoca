c99e3d8fefca111a11ea1f9f924737736bf02f0e
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-02-08 16:42:25 +0000

    [Add]admin 投稿管理機能作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,8 +2,13 @@ class Admin::PostsController < ApplicationController
   layout 'admin'
   
   def index
+    @posts = Post.page(params[:page])
+    @total_posts_count = Post.all.count
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.page(params[:page])
+    @total_comments_count = @post.comments.count
   end
 end
