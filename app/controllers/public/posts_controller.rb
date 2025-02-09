0bf6b6e8a2f6cbef0ed2a141905af230bdbddb40
EC2 Default User <ec2-user@ip-172-31-15-181.ap-northeast-1.compute.internal>
2025-02-09 09:00:21 +0000

    [Fix]管理者ページネーション表示、simple_format適用

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -41,6 +41,13 @@ class Public::PostsController < ApplicationController
       redirect_to post_path(@post)
     else
       flash.now[:danger] = "更新に失敗しました"
+      @total_comments_count = @post.comments.all.count
+      @new_programs = Program.published.order(created_at: :desc).limit(3)
+      if @post.comments.present?
+        @comments = @post.comments.page(params[:comments_page]).per(6)
+      else
+        @comments = Comment.none.page(params[:comments_page]).per(6)
+      end
       render :edit
     end
   end
